c6e8bfdf4a7a480b3039702d1231312c76c9d7b5
MihailTsybakov <62279777+MihailTsybakov@users.noreply.github.com>
2021-05-19 17:37:07 +0300

    Complete rewrite & optimization: new architecture

--- network.cpp
+++ network.cpp
@@ -1,250 +1,163 @@
 #include "network.h"
 
-network::network(int perception_neurons, std::vector<int> hidden_layers, int output_neurons)
+network::network(create_instructor ci)
 {
-    // hidden_layers.size() = number of hidden layers, each vector element = size of hidden layer
-    input_layer.resize(perception_neurons);
-    int tmp_layer_dim = perception_neurons;
-    for (size_t i = 0; i < hidden_layers.size(); ++i)
-    {
-        layers.push_back(network_layer(hidden_layers[i], tmp_layer_dim));
-        tmp_layer_dim = hidden_layers[i];
-    }
-    layers.push_back(network_layer(output_neurons, tmp_layer_dim));
+	this->activation = ci.activation;
+	this->d_activation = ci.d_activation;
+	int prevlayer_size = ci.perception_neurons;
+	for (auto layer_size : ci.hidden_layers)
+	{
+		layers.push_back(layer(layer_size, prevlayer_size));
+		prevlayer_size = layer_size;
+	}
+	layers.push_back(layer(ci.output_neurons, prevlayer_size));
 }
 
-void network::store(std::string path, std::string filename_template) const
+void network::store(std::string network_name) const
 {
-    for (size_t i = 0; i < layers.size(); ++i)
-    {
-        std::stringstream ss;
-        ss << path << "\\" << filename_template << "_" << i << ".txt";
-        layers[i].store_layer(ss.str());
-    }
-    std::cout << "<logs> Network stored." << std::endl;
+	for (size_t i = 0; i < layers.size(); ++i)
+	{
+		std::stringstream filename;
+		filename << network_name << "_" << i << ".txt";
+		layers[i].save(filename.str());
+	}
+	std::cout << "<logs> Network stored." << std::endl;
 }
 
-void network::load(std::string path, std::string filename_template, int layer_num)
+void network::load(std::string network_name, int layer_count)
 {
-    for (int i = 0; i < layer_num; ++i)
-    {
-        std::stringstream ss;
-        ss << path << "\\" << filename_template << "_" << i << ".txt";
-        layers[i].load_layer(ss.str());
-    }
-    std::cout << "<logs> Network loaded." << std::endl;
+	layers.resize(layer_count);
+	for (int i = 0; i < layer_count; ++i)
+	{
+		std::stringstream filename;
+		filename << network_name << "_" << i << ".txt";
+		layers[i].load(filename.str());
+	}
+	std::cout << "<logs> Network loaded." << std::endl;
 }
 
-std::pair<std::vector<double>, std::vector<double>> network::feedforward_step(int l, std::vector<double> previous_activations)
+
+std::vector<double> network::feedforward(std::vector<double> perception)
 {
-    // Returns current weighted sum (pair.first) and current neuron activations (pair.second)
-    std::vector<double> curr_WS = layers[l].W.mv_mult(previous_activations) + layers[l].bias; // Weighted sums
-    std::vector<double> current_activations = apply_f(curr_WS, activation); // Current neuron layer activations
-    return std::pair<std::vector<double>, std::vector<double>>(curr_WS, current_activations);
+	for (auto layer : layers) perception = apply(layer.W.mult_by_v(perception) + layer.bias, activation);
+	return perception;
 }
 
-std::vector<double> network::backpropagation_step(int l, std::vector<double> weighted_sum, std::vector<double> next_delta)
+
+void network::sgd_step(sgd_instructor si)
 {
-    // Calculates layer l's error (= delta) with weighted sum and delta of layer l+1
-    std::vector<double> dsigma = apply_f(weighted_sum, d_activation);  // d_
-    matrix transposed_weights = (layers[l + 1].W).transpose();
-    std::vector<double> matrix_product = transposed_weights.mv_mult(next_delta);
-    std::vector<double> delta_l = hadamard_product(matrix_product, dsigma);
-    return delta_l;
+	std::vector<std::vector<double>> neuron_inputs, neuron_activations, layer_deltas;
+	std::vector<double> activations = si.perception;
+	for (auto layer : layers) // feedforward
+	{
+		std::vector<double> currlayer_inputs = layer.W.mult_by_v(activations) + layer.bias;
+		neuron_inputs.push_back(currlayer_inputs);
+		neuron_activations.push_back(apply(currlayer_inputs, activation));
+		activations = apply(currlayer_inputs, activation);
+	}
+	int lnum = static_cast<int>(layers.size());
+	layer_deltas.resize(lnum);
+	layer_deltas[lnum - 1] = hadamard_product(apply(neuron_inputs[lnum - 1], d_activation), (neuron_activations[lnum - 1] - si.true_ans));
+	activations = si.perception;
+	for (int i = lnum - 2; i >= 0; --i) // backpropagate
+	{
+		layer_deltas[i] = hadamard_product(apply(neuron_inputs[i], d_activation), layers[i + 1].W.transpose().mult_by_v(layer_deltas[i + 1]));
+	}
+	for (int i = 0; i < lnum; ++i) // updating weights and biases
+	{
+		si.mtx->lock();
+		for (size_t j = 0; j < layers[i].layer_size; ++j) layers[i].bias[j] -= (si.learning_rate / si.batch_size) * layer_deltas[i][j];
+		for (size_t p = 0; p < layers[i].layer_size; ++p)
+		{
+			for (size_t q = 0; q < layers[i].prev_size; ++q)
+			{
+				layers[i].W.M[p][q] -= (si.learning_rate / si.batch_size) * (activations[q] * layer_deltas[i][p]);
+			}
+		}
+		si.mtx->unlock();
+		activations = neuron_activations[i]; // ~~
+	}
 }
 
-std::vector<double> network::lastlayer_delta(std::vector<double> true_ans, std::vector<double> activations, std::vector<double> weighted_sum)
+void network::sgd_learn(learn_instructor li)
 {
-    std::vector<double> ll_delta = hadamard_product((activations - true_ans), apply_f(weighted_sum, d_activation)); // d_
-    return ll_delta;
+	srand(time(nullptr));
+	int lr_upd = 0;
+	for (int epoch = 0; epoch < li.epoch_count; ++epoch)
+	{
+		lr_upd++;
+		if (li.epoch_logs == true)
+		{
+			li.mtx->lock();
+			std::cout << "<logs> Thread " << std::this_thread::get_id() << ": starting epoch " << epoch + 1 << std::endl;
+			std::cout << "<logs> Thread " << std::this_thread::get_id() << ": accuracy is " << batch_test(li.test_images, li.test_answers) << std::endl;
+			li.mtx->unlock();
+		}
+		if (li.dynamic_LR == true && lr_upd >= li.update_frequency)
+		{
+			lr_upd = 0;
+			li.learning_rate *= li.mult_factor;
+		}
+
+		for (int batch = 0; batch < li.train_images.size() / li.batch_size; ++batch)
+		{
+			for (int sample = 0; sample < li.batch_size; ++sample)
+			{
+				int rand_index = rand() % li.train_answers.size();
+				std::vector<double> true_answer; true_answer.resize(10);
+				for (int i = 0; i < layers[layers.size() - 1].layer_size; ++i) true_answer[i] = 0.0;
+				true_answer[li.train_answers[rand_index]] = 1.0;
+				sgd_instructor si; si.batch_size = li.batch_size;
+				si.L2_lambda = li.L2_lambda; si.learning_rate = li.learning_rate;
+				si.perception = form_perception(li.train_images[rand_index]);
+				si.true_ans = true_answer; si.mtx = li.mtx;
+
+				sgd_step(si);
+			}
+		}
+	}
 }
 
-std::vector<double> network::form_perception(digit_container DC)
+void network::sgd_parallel_learn(learn_instructor li)
 {
-    std::vector<double> perc;
-    int h = DC.dim().first;
-    int w = DC.dim().second;
-    perc.resize(h * w);
-    for (int i = 0; i < h; ++i)
-    {
-        for (int j = 0; j < w; ++j)
-        {
-            perc[i * w + j] = DC.get(j, i);
-        }
-    }
-    return perc;
+	std::mutex* sgd_mutex = new std::mutex;
+	std::vector<std::thread> thread_pool;
+	if (li.epoch_count % li.thread_number != 0) std::cout << "Error: epoch number must be completely divided by thread number." << std::endl, exit(-1);
+	learn_instructor li_; 
+	
+	li_.batch_size = li.batch_size; li_.dynamic_LR = li.dynamic_LR;
+	li_.epoch_count = li.epoch_count / li.thread_number; li_.epoch_logs = li.epoch_logs;
+	li_.L2_lambda = li.L2_lambda; li_.learning_rate = li.learning_rate; li_.mtx = sgd_mutex;
+	li_.mult_factor = li.mult_factor; li_.test_answers = li.test_answers; li_.test_images = li.test_images;
+	li_.thread_number = 0; li_.train_answers = li.train_answers; li_.train_images = li.train_images;
+	li_.update_frequency = li.update_frequency;
+
+	if (li.epoch_logs == true) std::cout << "<logs> Invoking thread pool." << std::endl;
+	for (int i = 0; i < li.thread_number; ++i) thread_pool.push_back(std::thread(&network::sgd_learn, this, li_));
+	for (auto th = thread_pool.begin(); th != thread_pool.end(); ++th) th->join();
+	if (li.epoch_logs == true) std::cout << "<logs> Thread pool joined." << std::endl;
+
+	delete sgd_mutex;
 }
 
-std::vector<double> network::raw_identify(digit_container DC) // Returns Probability vector
+int network::identify(digit_container dc)
 {
-    // Reading data from digit container
-    std::vector<double> input_perception = form_perception(DC);
-    std::vector<double> tmp_activations;
-    tmp_activations = input_perception;
-    for (size_t i = 0; i < layers.size(); ++i)
-    {
-        std::pair<std::vector<double>, std::vector<double>> fws_result = feedforward_step(i, tmp_activations);
-        tmp_activations = fws_result.second;
-    }
-    return tmp_activations;
+	std::vector<double> conf_vect = feedforward(form_perception(dc));
+	int choosen_digit = 0;
+	for (int i = 0; i < layers[layers.size() - 1].layer_size; ++i)
+	{
+		if (conf_vect[i] > conf_vect[choosen_digit]) choosen_digit = i;
+	}
+	return choosen_digit;
 }
 
-int network::identify(digit_container DC) // Returns identified digit
+double network::batch_test(std::vector<digit_container> imgs, std::vector<int> labels)
 {
-    std::vector<double> digits_probabilities = raw_identify(DC);
-    int choosen_digit = 0;
-    for (int i = 0; i < 10; ++i)
-    {
-        if (digits_probabilities[i] > digits_probabilities[choosen_digit])
-        {
-            choosen_digit = i;
-        }
-    }
-    return choosen_digit;
-}
-
-double network::batch_test(std::vector<digit_container> batch, std::vector<int> true_answers)
-{
-    int count = 0;
-    for (size_t i = 0; i < true_answers.size(); ++i)
-    {
-        if (identify(batch[i]) == true_answers[i]) count++;
-    }
-    return static_cast<double>(count) / true_answers.size();
-}
-
-void network::SGD_learn(std::vector<digit_container> trainig_samples, std::vector<int> true_answers, double learning_rate,
-     int epoch_count, int batch_size, bool epoch_logs, bool dynamic_gamma, double gamma_factor, int update_frequency,
-     std::vector<digit_container> test_batch, std::vector<int> test_answers, double L2_lambda, std::mutex& mtx)
-{
-    if (true_answers.size() != trainig_samples.size())
-    {
-        std::cout << "Error: dimension mismatch in SGD." << std::endl;
-        exit(-1);
-    }
-    srand(time(nullptr));
-    int gamma_upd = 0;
-    for (int epoch_num = 0; epoch_num < epoch_count; ++epoch_num)
-    {
-        gamma_upd++;
-        // Logging
-        if (epoch_logs == true)
-        {
-            mtx.lock();
-            std::cout << "<logs> Thread " << std::this_thread::get_id() << ": epoch " << epoch_num << " started. " << std::endl;
-            std::cout << "<logs> Thread " << std::this_thread::get_id() << ": accuracy at epoch start: " << batch_test(test_batch, test_answers) << std::endl;
-            mtx.unlock();
-        }
-        // Updating learning rate
-        if (dynamic_gamma == true && gamma_upd >= update_frequency)
-        {
-            learning_rate *= gamma_factor;
-            gamma_upd = 0;
-        }
-        // Forming mini-batches
-        for (int batch_num = 0; batch_num < trainig_samples.size() / batch_size; ++batch_num)
-        {
-            for (int sample_num = 0; sample_num < batch_size; ++sample_num)
-            {
-                int index = rand() % trainig_samples.size();
-                digit_container train_dc = trainig_samples[index];
-                std::vector<double> sgd_vect = sgd_vector(true_answers[index]);
-                std::vector<double> sgd_perc = form_perception(train_dc);
-                SGD_step(sgd_vect, sgd_perc, learning_rate, batch_size, L2_lambda/trainig_samples.size(), mtx);
-            }
-        }
-    }
-}
-
-void network::SGD_step(std::vector<double> true_ans,  // Stohastic Gradiend Descent learning step
-    std::vector<double> perception_input,
-    double learning_rate, int batch_size, double L2_lambda, std::mutex& mtx)
-{
-    std::vector<std::vector<double>> layer_activations;     // al
-    std::vector<std::vector<double>> layer_weighted_sums;   // zl
-    std::vector<std::vector<double>> layer_deltas;          // dl
-    std::vector<double> tmp_activations = perception_input;
-    // feeding perception forward through net
-    for (size_t i = 0; i < layers.size(); ++i)
-    {
-        std::pair<std::vector<double>, std::vector<double>> fws_result = feedforward_step(i, tmp_activations);
-        layer_weighted_sums.push_back(fws_result.first);
-        layer_activations.push_back(fws_result.second);
-        tmp_activations = fws_result.second;
-    }
-    std::vector<double> ll_delta = lastlayer_delta(true_ans, tmp_activations, layer_weighted_sums[layers.size() - 1]);
-    layer_deltas.resize(layers.size());
-    layer_deltas[layers.size() - 1] = ll_delta; // Definig last layer's delta
-    for (int i = static_cast<int>(layers.size()) - 2; i >= 0; --i)
-    {
-        layer_deltas[i] = backpropagation_step(i, layer_weighted_sums[i], layer_deltas[i + 1]);
-    }
-    // Updating weights and biases:
-    for (size_t i = 0; i < layers.size(); ++i)
-    {
-        // Bias update:
-        mtx.lock();
-        for (size_t j = 0; j < layers[i].bias.size(); ++j) layers[i].bias[j] -= (learning_rate/batch_size) * layer_deltas[i][j];
-        mtx.unlock();
-        // Weights update:
-        std::vector<std::vector<double>> old_weights = layers[i].W.get_m();
-        std::vector<double> activations;
-        if (i == 0)
-        {
-            activations = perception_input;
-        }
-        else
-        {
-            activations = layer_activations[i - 1];
-        }
-        mtx.lock();
-        for (int p_ = 0; p_ < layers[i].W.dim().first; ++p_)
-        {
-            for (int q_ = 0; q_ < layers[i].W.dim().second; ++q_)
-            {    
-                layers[i].W.set(p_, q_, old_weights[p_][q_]*(1 - learning_rate*L2_lambda) - 
-                    (learning_rate/batch_size)*activations[q_]*layer_deltas[i][p_]);
-            }
-        }
-        mtx.unlock();
-    }
-}
-
-std::vector<double> network::sgd_vector(int digit)
-{
-    std::vector<double> res; res.resize(10);
-    for (int i = 0; i < 10; ++i) res[i] = 0.0;
-    res[digit] = 1.0;
-    return res;
-}
-
-void network::SGD_parallel_learn(std::vector<digit_container> training_samples, std::vector<int> true_answers, double learning_rate,
-     int epoch_count, int batch_size, bool epoch_logs, bool dynamic_gamma, double gamma_factor, int update_frequency,
-     std::vector<digit_container> test_batch, std::vector<int> test_answers, double L2_lambda, int thread_number)
-{
-    std::vector<std::thread> thread_pool;
-    std::mutex upd_mutex;
-    if (epoch_count % thread_number != 0)
-    {
-        std::cout << "Error: epoch count must be completely divided by thread number." << std::endl;
-        exit(-1);
-    }
-    if (epoch_logs == true)
-    {
-        std::cout << "<logs> Invoking thread pool." << std::endl;
-    }
-    for (int i = 0; i < thread_number; ++i)
-    {
-        thread_pool.push_back(std::thread(&network::SGD_learn, this, training_samples, true_answers, learning_rate,
-            epoch_count/thread_number, batch_size, epoch_logs, dynamic_gamma, gamma_factor, update_frequency, test_batch,
-            test_answers, L2_lambda, std::ref(upd_mutex)));
-    }
-    for (auto th = thread_pool.begin(); th != thread_pool.end(); ++th)
-    {
-        th->join();
-    }
-    if (epoch_logs == true)
-    {
-        std::cout << "<logs> Thread pool joined." << std::endl;
-    }
+	if (imgs.size() != labels.size()) std::cout << "Batch test failed: dimension mismatch." << std::endl, exit(-1);
+	int correct_answers = 0;
+	for (size_t i = 0; i < imgs.size(); ++i)
+	{
+		if (identify(imgs[i]) == labels[i]) correct_answers++;
+	}
+	return static_cast<double>(correct_answers) / imgs.size();
 }
