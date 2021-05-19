f49da7372b90ccccfd9330d9f63439ebc04d1f43
MihailTsybakov <62279777+MihailTsybakov@users.noreply.github.com>
2021-05-19 17:35:43 +0300

    Complete rewrite & optimization

--- matrix.cpp
+++ matrix.cpp
@@ -1,84 +1,54 @@
 #include "matrix.h"
 
-void matrix::print() const
+matrix::matrix(){}
+
+matrix::matrix(size_t rows, size_t cols)
 {
-    for (int i = 0; i < dim_h; ++i)
-    {
-        for (int j = 0; j < dim_w; ++j) std::cout << M[i][j] << " ";
-        std::cout << std::endl;
-    }
+	M.resize(rows); 
+	row_dim = rows;
+	col_dim = cols;
+	for (auto row = M.begin(); row != M.end(); ++row) row->resize(cols);
+}
+
+void matrix::random_fill(double mean, double sigma)
+{
+	for (size_t row = 0; row < M.size(); ++row)
+	{
+		for (size_t col = 0; col < M[0].size(); ++col) M[row][col] = random_number(0, 1 / sqrt(col_dim));
+	}
 }
 
 matrix matrix::transpose() const
 {
-    matrix res(dim_w, dim_h);
-    for (int i = 0; i < dim_h; ++i)
-    {
-        for (int j = 0; j < dim_w; ++j) res.set(j, i, M[i][j]);
-    }
-    return res;
+	matrix r_matrix(col_dim, row_dim);
+	for (size_t row = 0; row < row_dim; ++row)
+	{
+		for (size_t col = 0; col < col_dim; ++col) r_matrix.M[col][row] = M[row][col];
+	}
+	return r_matrix;
 }
 
-std::pair<int, int> matrix::dim() const
+std::vector<double> matrix::mult_by_v(std::vector<double> v) const
 {
-    return std::pair<int, int>(dim_h, dim_w);
+	if (v.size() != col_dim) std::cout << "Error: dimension mismatch in mult by vector." << std::endl, exit(-1);
+	std::vector<double> r_vect;
+	for (auto row : M) r_vect.push_back(scalar_product(row, v));
+	return r_vect;
 }
 
-matrix::matrix(int h, int w)
+void matrix::resize(size_t new_rows, size_t new_cols)
 {
-    M.resize(h);
-    for (int i = 0; i < h; ++i) M[i].resize(w);
-    dim_h = h;
-    dim_w = w;
+	row_dim = new_rows;
+	col_dim = new_cols;
+	M.resize(new_rows);
+	for (auto row = M.begin(); row != M.end(); ++row) row->resize(new_cols);
 }
 
-void matrix::random_fill()
+void matrix::print() const
 {
-    for (int i = 0; i < dim_h; ++i)
-    {
-        for (int j = 0; j < dim_w; ++j)
-        {
-            M[i][j] = random_number(0, 1/sqrt(dim_w));
-        }
-    }
-}
-
-void matrix::resize(int h, int w)
-{
-    M.resize(h);
-    dim_h = h;
-    for (int i = 0; i < h; ++i) M[i].resize(w);
-    dim_w = w;
-}
-
-void matrix::set(int i, int j, double val)
-{
-    M[i][j] = val;
-}
-
-void matrix::set_m(std::vector<std::vector<double>> M)
-{
-    dim_h = static_cast<int>(M.size());
-    dim_w = static_cast<int>(M[0].size());
-    this->M = M;
-}
-
-std::vector<std::vector<double>> matrix::get_m() const
-{
-    return M;
-}
-
-std::vector<double> matrix::mv_mult(std::vector<double> v)
-{
-    if (dim_w != v.size())
-    {
-        std::cout << "Error: wrong dimensions in mv_mult." << std::endl;
-        exit(-1);
-    }
-    std::vector<double> res;
-    for (int i = 0; i < dim_h; ++i)
-    {
-        res.push_back(scalmul(v, M[i]));
-    }
-    return res;
+	for (auto row : M)
+	{
+		for (auto element : row) std::cout << element << " ";
+		std::cout << std::endl;
+	}
 }
